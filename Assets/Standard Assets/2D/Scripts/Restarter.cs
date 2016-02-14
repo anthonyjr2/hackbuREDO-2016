6ceb9ba3de03e046d0c54a2fde29eaf41b4bbe73
kbaker8 <gilly2312@gmail.com>
2016-02-14 02:47:02 -0500

    made game over screen appear somewhat

--- Assets/Standard Assets/2D/Scripts/Restarter.cs
+++ Assets/Standard Assets/2D/Scripts/Restarter.cs
@@ -6,12 +6,19 @@ namespace UnityStandardAssets._2D
 {
     public class Restarter : MonoBehaviour
     {
+        
         private void OnTriggerEnter2D(Collider2D other)
         {
+           
             if (other.tag == "Player")
             {
                 print("dope");
                 other.transform.position = new Vector3(0, 1.1f, 0);
+                GameObject.Find("EndScreen").transform.position = new Vector2(1, 4);
+                GameObject.Find("JohnCena").GetComponent<UnityStandardAssets._2D.Platformer2DUserControl>().enabled = false;
+                //GetComponent<UnityStandardAssets._2D.Platformer2DUserControl>().enabled = false;
+
+
             }
         }
     }
